4c5f60649e4b12c9bd353de7bf7fb815a10f0dc7
Nikita Drimanovich <Nikita.Drimanovich@ya.ru>
2020-07-30 19:36:54 +0300

    clone contain-google

--- background.js
+++ background.js
@@ -1,22 +1,58 @@
 // Param values from https://developer.mozilla.org/Add-ons/WebExtensions/API/contextualIdentities/create
-const YANDEX_CONTAINER_NAME = "Yandex";
-const YANDEX_CONTAINER_COLOR = "yellow";
-const YANDEX_CONTAINER_ICON = "briefcase";
+const GOOGLE_CONTAINER_NAME = "Google";
+const GOOGLE_CONTAINER_COLOR = "red";
+const GOOGLE_CONTAINER_ICON = "briefcase";
 
-let YANDEX_NATIONAL_DOMAINS = ["yandex.it", "yandex.ru", "yandex.de", "yandex.com", "yandex.com.br", "yandex.in", "yandex.com.au", "yandex.es", "yandex.com.mx", "yandex.co.jp", "yandex.in", "yandex.co.uk", "yandex.ca", "yandex.fr", "yandex.com.sg"];
+let GOOGLE_DOMAINS = [
+  "google.com", "google.org", "googleapis.com", "g.co", "ggpht.com",
+  "blogger.com", "googleblog.com", "blog.google", "googleusercontent.com", "googlesource.com",
+  "google.org", "google.net", "466453.com", "gooogle.com", "gogle.com", "ggoogle.com", "gogole.com", "goolge.com", "googel.com", "googlee.com", "googil.com", "googlr.com", "elgoog.im", "ai.google", "com.google", "about.google", "registry.google",
+];
 
-let YANDEX_DOMAINS = ["yandex.company", "yandex.express", "yandex.gd", "yandex.international", "yandex.ltda", "yandex.press", "yandex.shopping", "yandex.tickets", "yandex.tv", "yandex.cruises", "yandex.dog", "yandex.express", "yandex.game", "yandex.gent", "yandex.salon", "yandex.shopping", "yandex.tours", "yandex.wiki", "yandex.clothing", "yandex.energy", "yandex.fund", "yandex.hockey", "yandex.kiwi", "yandex.re", "yandex.soccer", "yandex.tienda", "yandex.training", "yandex.jobs", "yandexpay.in", "yandexteam.org"];
+const GOOGLE_INTL_DOMAINS = [
+  "google.ac", "google.ad", "google.ae", "google.com.af", "google.com.ag", "google.com.ai", "google.al", "google.am", "google.co.ao", "google.com.ar", "google.as", "google.at", "google.com.au", "google.az", "google.ba", "google.com.bd", "google.be", "google.bf", "google.bg", "google.com.bh", "google.bi", "google.bj", "google.com.bn", "google.com.bo", "google.com.br", "google.bs", "google.bt", "google.com.bw", "google.by", "google.com.bz", "google.ca", "google.com.kh", "google.cc", "google.cd", "google.cf", "google.cat", "google.cg", "google.ch", "google.ci", "google.co.ck", "google.cl", "google.cm", "google.cn", "google.com.co", "google.co.cr", "google.com.cu", "google.cv", "google.com.cy", "google.cz", "google.de", "google.dj", "google.dk", "google.dm", "google.com.do", "google.dz", "google.com.ec", "google.ee", "google.com.eg", "google.es", "google.com.et", "google.fi", "google.com.fj", "google.fm", "google.fr", "google.ga", "google.ge", "google.gf", "google.gg", "google.com.gh", "google.com.gi", "google.gl", "google.gm", "google.gp", "google.gr", "google.com.gt", "google.gy", "google.com.hk", "google.hn", "google.hr", "google.ht", "google.hu", "google.co.id", "google.iq", "google.ie", "google.co.il", "google.im", "google.co.in", "google.io", "google.is", "google.it", "google.je", "google.com.jm", "google.jo", "google.co.jp", "google.co.ke", "google.ki", "google.kg", "google.co.kr", "google.com.kw", "google.kz", "google.la", "google.lb", "google.com.lc", "google.li", "google.lk", "google.co.ls", "google.lt", "google.lu", "google.lv", "google.com.ly", "google.co.ma", "google.md", "google.me", "google.mg", "google.mk", "google.ml", "google.com.mm", "google.mn", "google.ms", "google.com.mt", "google.mu", "google.mv", "google.mw", "google.com.mx", "google.com.my", "google.co.mz", "google.com.na", "google.ne", "google.com.nf", "google.com.ng", "google.com.ni", "google.nl", "google.no", "google.com.np", "google.nr", "google.nu", "google.co.nz", "google.com.om", "google.com.pk", "google.com.pa", "google.com.pe", "google.com.ph", "google.pl", "google.com.pg", "google.pn", "google.com.pr", "google.ps", "google.pt", "google.com.py", "google.com.qa", "google.ro", "google.rs", "google.ru", "google.rw", "google.com.sa", "google.com.sb", "google.sc", "google.se", "google.com.sg", "google.sh", "google.si", "google.sk", "google.com.sl", "google.sn", "google.sm", "google.so", "google.st", "google.sr", "google.com.sv", "google.td", "google.tg", "google.co.th", "google.com.tj", "google.tk", "google.tl", "google.tm", "google.to", "google.tn", "google.com.tr", "google.tt", "google.com.tw", "google.co.tz", "google.com.ua", "google.co.ug", "google.co.uk", "google.us", "google.com.uy", "google.co.uz", "google.com.vc", "google.co.ve", "google.vg", "google.co.vi", "google.com.vn", "google.vu", "google.ws", "google.co.za", "google.co.zm", "google.co.zw",
+];
 
-YANDEX_DOMAINS = YANDEX_DOMAINS.concat(YANDEX_NATIONAL_DOMAINS);
+const GOOGLE_SERVICES = [
+"like.com", "keyhole.com", "panoramio.com", "picasa.com", "urchin.com", "igoogle.com", "foofle.com", "froogle.com", "localguidesconnect.com", "googlemail.com", "googleanalytics.com", "google-analytics.com", "googletagmanager.com", "googlecode.com", "googlesource.com", "googledrive.com", "googlearth.com", "googleearth.com", "googlemaps.com", "googlepagecreator.com", "googlescholar.com", "advertisercommunity.com", "thinkwithgoogle.com", "googlegroups.com",
+];
+
+const YOUTUBE_DOMAINS = [
+  "youtube.com", "youtu.be", "yt.be", "ytimg.com", "youtube-nocookie.com", "youtubegaming.com", "youtubeeducation.com",
+];
+
+const BLOGSPOT_DOMAINS = [
+  "blogspot.com", "blogspot.ae", "blogspot.al", "blogspot.am", "blogspot.com.ar", "blogspot.co.at", "blogspot.com.au", "blogspot.ba", "blogspot.be", "blogspot.bg", "blogspot.bj", "blogspot.com.br", "blogspot.com.by", "blogspot.ca", "blogspot.cf", "blogspot.ch", "blogspot.cl", "blogspot.com.co", "blogspot.cv", "blogspot.com.cy", "blogspot.cz", "blogspot.de", "blogspot.dj", "blogspot.dk", "blogspot.dm", "blogspot.com.do", "blogspot.dz", "blogspot.com.eg", "blogspot.es", "blogspot.fi", "blogspot.fr", "blogspot.gr", "blogspot.hr", "blogspot.hu", "blogspot.co.id", "blogspot.ie", "blogspot.co.il", "blogspot.in", "blogspot.is", "blogspot.it", "blogspot.jp", "blogspot.co.ke", "blogspot.kr", "blogspot.li", "blogspot.lt", "blogspot.lu", "blogspot.md", "blogspot.mk", "blogspot.com.mt", "blogspot.mx", "blogspot.my", "blogspot.com.ng", "blogspot.nl", "blogspot.no", "blogspot.co.nz", "blogspot.pt", "blogspot.qa", "blogspot.ro", "blogspot.rs", "blogspot.ru", "blogspot.se", "blogspot.sg", "blogspot.si", "blogspot.sk", "blogspot.sn", "blogspot.com.sr", "blogspot.td", "blogspot.co.tl", "blogspot.co.to", "blogspot.com.tr", "blogspot.tw", "blogspot.co.uk", "blogspot.com.uy", "blogspot.co.za", "stackdriver.com",
+];
+
+const ALPHABET_DOMAINS = [
+  "abc.xyz", "waze.com", "capitalg.com", "gv.com", "calicolabs.com", "x.company", "nest.com", "sidewalklabs.com", "verily.com",
+];
+
+const AD_DOMAINS = [
+  "doubleclickbygoogle.com", "feedburner.com", "doubleclick.com", "doubleclick.net", "adwords.com", "adsense.com", "admob.com", "advertisercommunity.com",
+  "googlesyndication.com", "googlecommerce.com", "googlebot.com", "googleapps.com", "googleadservices.com", "gmodules.com", "googl.com",
+  "1e100.net", "domains.google", "gv.com", "googletraveladservices.com",
+];
+
+const DEVELOPER_DOMAINS = [
+  "madewithcode.com", "design.google", "wellbeing.google", "gallery.io", "domains.google", "material.io", "getmdl.io", "android.com", "chromium.org", "cobrasearch.com", "chromecast.com", "chrome.com", "chromebook.com", "madewithcode.com", "whatbrowser.org", "withgoogle.com", "web.dev",
+];
+
+GOOGLE_DOMAINS = GOOGLE_DOMAINS.concat(GOOGLE_INTL_DOMAINS)
+  .concat(GOOGLE_SERVICES).concat(YOUTUBE_DOMAINS).concat(BLOGSPOT_DOMAINS).concat(ALPHABET_DOMAINS)
+  .concat(DEVELOPER_DOMAINS).concat(AD_DOMAINS);
 
 const MAC_ADDON_ID = "@testpilot-containers";
 
 let macAddonEnabled = false;
-let yandexCookieStoreId = null;
+let googleCookieStoreId = null;
+let extensionSettings = {};
 
 const canceledRequests = {};
 const tabsWaitingToLoad = {};
-const yandexHostREs = [];
+const googleHostREs = [];
+const youtubeHostREs = [];
 
 async function isMACAddonEnabled () {
   try {
@@ -112,14 +148,25 @@ function shouldCancelEarly (tab, options) {
   return false;
 }
 
-function generateYandexHostREs () {
-  for (let yandexDomain of YANDEX_DOMAINS) {
-    yandexHostREs.push(new RegExp(`^(.*\\.)?${yandexDomain}$`));
+function generateGoogleHostREs () {
+  const matchOperatorsRegex = /[|\\{}()[\]^$+*?.-]/g;
+
+  for (let googleDomain of GOOGLE_DOMAINS) {
+    googleDomain = googleDomain.replace(matchOperatorsRegex, '\\$&');
+    googleHostREs.push(new RegExp(`(^|\.)${googleDomain}$`));
+  }
+  for (let youtubeDomain of YOUTUBE_DOMAINS) {
+    youtubeDomain = youtubeDomain.replace(matchOperatorsRegex, '\\$&');
+    youtubeHostREs.push(new RegExp(`(^|\.)${youtubeDomain}$`));
   }
 }
 
-async function clearYandexCookies () {
-  // Clear all yandex cookies
+async function loadExtensionSettings () {
+  extensionSettings = await browser.storage.sync.get();
+}
+
+async function clearGoogleCookies () {
+  // Clear all google cookies
   const containers = await browser.contextualIdentities.query({});
   containers.push({
     cookieStoreId: "firefox-default"
@@ -127,39 +174,39 @@ async function clearYandexCookies () {
 
   let macAssignments = [];
   if (macAddonEnabled) {
-    const promises = YANDEX_DOMAINS.map(async yandexDomain => {
-      const assigned = await getMACAssignment(`https://${yandexDomain}/`);
-      return assigned ? yandexDomain : null;
+    const promises = GOOGLE_DOMAINS.map(async googleDomain => {
+      const assigned = await getMACAssignment(`https://${googleDomain}/`);
+      return assigned ? googleDomain : null;
     });
     macAssignments = await Promise.all(promises);
   }
 
-  YANDEX_DOMAINS.map(async yandexDomain => {
-    const yandexCookieUrl = `https://${yandexDomain}/`;
+  GOOGLE_DOMAINS.map(async googleDomain => {
+    const googleCookieUrl = `https://${googleDomain}/`;
 
-    // dont clear cookies for yandexDomain if mac assigned (with or without www.)
+    // dont clear cookies for googleDomain if mac assigned (with or without www.)
     if (macAddonEnabled &&
-        (macAssignments.includes(yandexDomain) ||
-         macAssignments.includes(`www.${yandexDomain}`))) {
+        (macAssignments.includes(googleDomain) ||
+         macAssignments.includes(`www.${googleDomain}`))) {
       return;
     }
 
     containers.map(async container => {
       const storeId = container.cookieStoreId;
-      if (storeId === yandexCookieStoreId) {
-        // Don't clear cookies in the Yandex Container
+      if (storeId === googleCookieStoreId) {
+        // Don't clear cookies in the Google Container
         return;
       }
 
       const cookies = await browser.cookies.getAll({
-        domain: yandexDomain,
+        domain: googleDomain,
         storeId
       });
 
       cookies.map(cookie => {
         browser.cookies.remove({
           name: cookie.name,
-          url: yandexCookieUrl,
+          url: googleCookieUrl,
           storeId
         });
       });
@@ -168,17 +215,17 @@ async function clearYandexCookies () {
 }
 
 async function setupContainer () {
-  // Use existing Yandex container, or create one
-  const contexts = await browser.contextualIdentities.query({name: YANDEX_CONTAINER_NAME});
+  // Use existing Google container, or create one
+  const contexts = await browser.contextualIdentities.query({name: GOOGLE_CONTAINER_NAME});
   if (contexts.length > 0) {
-    yandexCookieStoreId = contexts[0].cookieStoreId;
+    googleCookieStoreId = contexts[0].cookieStoreId;
   } else {
     const context = await browser.contextualIdentities.create({
-      name: YANDEX_CONTAINER_NAME,
-      color: YANDEX_CONTAINER_COLOR,
-      icon: YANDEX_CONTAINER_ICON
+      name: GOOGLE_CONTAINER_NAME,
+      color: GOOGLE_CONTAINER_COLOR,
+      icon: GOOGLE_CONTAINER_ICON
     });
-    yandexCookieStoreId = context.cookieStoreId;
+    googleCookieStoreId = context.cookieStoreId;
   }
 }
 
@@ -187,20 +234,49 @@ function reopenTab ({url, tab, cookieStoreId}) {
     url,
     cookieStoreId,
     active: tab.active,
-    index: tab.index,
+    index: tab.index + 1,
     windowId: tab.windowId
   });
-  browser.tabs.remove(tab.id);
+  // We do not want to erase google container if going from 
+  // google container back to default.
+  if (!(isSearchPageURL(tab.url))) {
+    browser.tabs.remove(tab.id);
+  }
 }
 
-function isYandexURL (url) {
+function isGoogleURL (url) {
   const parsedUrl = new URL(url);
-  for (let yandexHostRE of yandexHostREs) {
-    if (yandexHostRE.test(parsedUrl.host)) {
+  for (let googleHostRE of googleHostREs) {
+    if (googleHostRE.test(parsedUrl.host)) {
       return true;
     }
   }
   return false;
+}
+
+function isYouTubeURL (url) {
+  const parsedUrl = new URL(url);
+  for (let youtubeHostRE of youtubeHostREs) {
+    if (youtubeHostRE.test(parsedUrl.host)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isSearchPageURL (url) {
+  const parsedUrl = new URL(url);
+  return parsedUrl.pathname.startsWith('/search');
+}
+
+function isMapsURL (url) {
+  const parsedUrl = new URL(url);
+  return parsedUrl.pathname.startsWith('/maps');
+}
+
+function isFlightsURL (url) {
+  const parsedUrl = new URL(url);
+  return parsedUrl.pathname.startsWith('/flights');
 }
 
 function shouldContainInto (url, tab) {
@@ -209,14 +285,32 @@ function shouldContainInto (url, tab) {
     return false;
   }
 
-  if (isYandexURL(url)) {
-    if (tab.cookieStoreId !== yandexCookieStoreId) {
-      // Yandex-URL outside of Yandex Container Tab
-      // Should contain into Yandex Container
-      return yandexCookieStoreId;
+  let handleUrl = isGoogleURL(url);
+
+  if (handleUrl && extensionSettings.ignore_youtube && isYouTubeURL(url)) {
+    handleUrl = false;
+  }
+
+  if (handleUrl && extensionSettings.ignore_searchpages && isSearchPageURL(url)) {
+    handleUrl = false;
+  }
+
+  if (handleUrl && extensionSettings.ignore_maps && isMapsURL(url)) {
+    handleUrl = false;
+  }
+
+  if (handleUrl && extensionSettings.ignore_flights && isFlightsURL(url)) {
+    handleUrl = false;
+  }
+
+  if (handleUrl) {
+    if (tab.cookieStoreId !== googleCookieStoreId) {
+      // Google-URL outside of Google Container Tab
+      // Should contain into Google Container
+      return googleCookieStoreId;
     }
-  } else if (tab.cookieStoreId === yandexCookieStoreId) {
-    // Non-Yandex-URL inside Yandex Container Tab
+  } else if (tab.cookieStoreId === googleCookieStoreId) {
+    // Non-Google-URL inside Google Container Tab
     // Should contain into Default Container
     return "firefox-default";
   }
@@ -286,8 +380,8 @@ async function maybeReopenAlreadyOpenTabs () {
   });
 }
 
-async function containYandex (options) {
-  // Listen to requests and open Yandex into its Container,
+async function containGoogle (options) {
+  // Listen to requests and open Google into its Container,
   // open other sites into the default tab context
   if (options.tabId === -1) {
     // Request doesn't belong to a tab
@@ -340,13 +434,14 @@ async function containYandex (options) {
   } catch (error) {
     // TODO: Needs backup strategy
     // See https://github.com/mozilla/contain-facebook/issues/23
-    // Sometimes this add-on is installed but doesn't get a yandexCookieStoreId ?
+    // Sometimes this add-on is installed but doesn't get a googleCookieStoreId ?
     // eslint-disable-next-line no-console
     console.log(error);
     return;
   }
-  clearYandexCookies();
-  generateYandexHostREs();
+  loadExtensionSettings();
+  clearGoogleCookies();
+  generateGoogleHostREs();
 
   // Clean up canceled requests
   browser.webRequest.onCompleted.addListener((options) => {
@@ -361,7 +456,7 @@ async function containYandex (options) {
   },{urls: ["<all_urls>"], types: ["main_frame"]});
 
   // Add the request listener
-  browser.webRequest.onBeforeRequest.addListener(containYandex, {urls: ["<all_urls>"], types: ["main_frame"]}, ["blocking"]);
+  browser.webRequest.onBeforeRequest.addListener(containGoogle, {urls: ["<all_urls>"], types: ["main_frame"]}, ["blocking"]);
 
   maybeReopenAlreadyOpenTabs();
 })();
